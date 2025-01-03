b817c40bc00a7fc407ace9841f6a49945dcd96f2
Samjhana Timsina <analyst.samjhana@gmail.com>
2025-01-03 12:21:37 -0600

    New Files Added

---
+++ TipCalculatorApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TipCalculatorApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TipCalculatorApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace TipCalculatorApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCalculateClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            double bill = Convert.ToDouble(BillEntry.Text);
+            double tipPercentage = Convert.ToDouble(TipPercentageEntry.Text);
+            double tip = bill * tipPercentage / 100;
+            double total = bill + tip;
+
+            TotalLabel.Text = $"Total: ${total:F2}";
+        }
+        catch (Exception)
+        {
+            TotalLabel.Text = "Please enter valid numbers.";
+        }
+    }
+}
